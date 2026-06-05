27b1c9151a07bb2ab398bc8ac3bd2150750ee5c1
Tahia Hoque <89339274+C191248TahiaHoque@users.noreply.github.com>
2026-06-05 21:36:18 +0600

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,7 @@ import hashlib
 import json
 import time
 
+
 class Block:
     def __init__(self, index, data, previous_hash):
         self.index = index
@@ -11,21 +12,63 @@ class Block:
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
-        block_string = json.dumps(self.__dict__, sort_keys=True)
+        block_data = {
+            "index": self.index,
+            "timestamp": self.timestamp,
+            "data": self.data,
+            "previous_hash": self.previous_hash
+        }
+
+        block_string = json.dumps(block_data, sort_keys=True)
+
         return hashlib.sha256(block_string.encode()).hexdigest()
-
-
+    
 class Blockchain:
+
     def __init__(self):
-        self.chain = [self.create_genesis_block()]
+
+        self.chain = []
+
+        self.create_genesis_block()
 
     def create_genesis_block(self):
-        return Block(0, {"msg": "Genesis Block"}, "0")
 
-    def get_latest_block(self):
-        return self.chain[-1]
+        genesis = Block(
+            0,
+            {
+                "user": "GENESIS",
+                "action": "genesis"
+            },
+            "0"
+        )
+
+        self.chain.append(genesis)
 
     def add_block(self, data):
-        prev_block = self.get_latest_block()
-        new_block = Block(len(self.chain), data, prev_block.hash)
+
+        previous_block = self.chain[-1]
+
+        new_block = Block(
+            len(self.chain),
+            data,
+            previous_block.hash
+        )
+
         self.chain.append(new_block)
+
+    def is_valid(self):
+
+        for i in range(1, len(self.chain)):
+
+            current = self.chain[i]
+            previous = self.chain[i - 1]
+
+            # Verify chain linkage
+            if current.previous_hash != previous.hash:
+                return False
+
+            # Verify current block hash
+            if current.calculate_hash() != current.hash:
+                return False
+
+        return True
